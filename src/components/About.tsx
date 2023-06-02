e05ecb1bdc8b5657eaf756f928f7fc809fe3319d
dev-pengi <siffffdoul@gmail.com>
2023-06-02 20:52:59 +0100

    Add key prop to the quote list

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,26 +14,28 @@ const About: FC = () => {
       <div
         className={`${contentPadding} max-w-[1600px] mx-auto flex lg:flex-row flex-col items-center justify-between mt-[10px]`}
       >
-          <div className="relative w-full max-w-[400px]">
-            <Image src={blob} alt="blob shape svg" className="w-full" />
-            <Image
-              src={dev}
-              alt="dev vector icon"
-              className="w-[150px] absolute top-0 bottom-0 right-0 left-0 m-auto block"
-            />
-          </div>
-          <p className="text-lg font-medium break-words whitespace-pre-wrap lg:text-start text-center">
-            {aboutDescription}
-          </p>
+        <div className="relative w-full max-w-[400px]">
+          <Image src={blob} alt="blob shape svg" className="w-full" />
+          <Image
+            src={dev}
+            alt="dev vector icon"
+            className="w-[150px] absolute top-0 bottom-0 right-0 left-0 m-auto block"
+          />
+        </div>
+        <p className="text-lg font-medium break-words whitespace-pre-wrap lg:text-start text-center">
+          {aboutDescription}
+        </p>
       </div>
 
-      <h3 className="capitalize font-black text-center text-3xl lg:mt-[20px] mt-[60px]">quotes</h3>
+      <h3 className="capitalize font-black text-center text-3xl lg:mt-[20px] mt-[60px]">
+        quotes
+      </h3>
 
       <div
         className={`${contentPadding} grid grid-cols-quote-card gap-9 mt-[60px] max-w-[1600px] mx-auto`}
       >
-        {quotes.map((quote) => (
-          <div className="bg-dark-3 px-[12px] py-[14px] rounded-md duration-300 hover:-translate-y-1">
+        {quotes.map((quote, index: number) => (
+          <div key={index} className="bg-dark-3 px-[12px] py-[14px] rounded-md duration-300 hover:-translate-y-1">
             <p>“{quote}”</p>
             <h5 className="mt-3 flex justify-end italic font-light text-[15px]">
               - sifedine
