1d6aef859e741e29534fae70b89c4e007b7c10c6
dev-pengi <siffffdoul@gmail.com>
2023-06-03 00:03:11 +0100

    Update the About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,15 +9,15 @@ import { Tilt } from "react-tilt";
 
 const About: FC = () => {
   const defaultTiltOptions = {
-    reverse: false, // reverse the tilt direction
-    max: 10, // max tilt rotation (degrees)
-    perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
-    scale: 1, // 2 = 200%, 1.5 = 150%, etc..
-    speed: 1000, // Speed of the enter/exit transition
-    transition: true, // Set a transition on enter/exit.
-    axis: 'Y', // What axis should be disabled. Can be X or Y.
-    reset: true, // If the tilt effect has to be reset on exit.
-    easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
+    reverse: false,
+    max: 10,
+    perspective: 1000,
+    scale: 1,
+    speed: 1000, 
+    transition: true,
+    axis: 'Y',
+    reset: true, 
+    easing: "cubic-bezier(.03,.98,.52,.99)",
   };
   return (
     <section className="bg-dark-2 mt-[150px] py-6" id="about">
