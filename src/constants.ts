58a4306e47e96a5fa27c619fe7eb87b9052f5724
dev-pengi <siffffdoul@gmail.com>
2023-06-02 20:45:19 +0100

    Update constants

--- src/constants.ts
+++ src/constants.ts
@@ -40,4 +40,13 @@ const headerItems: IheaderItem[] = [
   },
 ];
 
-export { name, headerItems, hero };
+const aboutDescription: string =
+  "i'm a young developer from algeria, i use javascript and typescript i code both client side (react, nextjs) and backend (nodejs express and mongodb) and even server hosting and management. i've shown a good level of dedication and perseverance in pursuing my passion for software development. with a natural ability to solve problems and an eagerness to learn, i'm committed to keeping up to date with the latest technology. my diverse skills in web development, server management, and publishing make me an invaluable asset to any team.";
+
+const quotes: string[] = [
+  "never regret the past, if the action was good it's a memory, if it wasn't then it's a lesson",
+  "the future isn't scary, the future isn't joyful. the future is the result of your very moment",
+  "if you're not facing problems at what you're doing, you are not evolving you are repeating",
+];
+
+export { name, headerItems, aboutDescription, hero, quotes };
