9c145a34e7f540128f12be3e76ff44aa38109140
dev-pengi <siffffdoul@gmail.com>
2023-06-02 22:26:20 +0100

    Added a tilt

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,10 +4,21 @@ import Image from "next/image";
 import blob from "../../public/blob.svg";
 import dev from "../../public/dev.png";
 import { contentPadding } from "@/styles";
-import { Fade } from "react-awesome-reveal";
 import { aboutDescription, quotes } from "@/constants";
+import { Tilt } from "react-tilt";
 
 const About: FC = () => {
+  const defaultTiltOptions = {
+    reverse: false, // reverse the tilt direction
+    max: 10, // max tilt rotation (degrees)
+    perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+    scale: 1, // 2 = 200%, 1.5 = 150%, etc..
+    speed: 1000, // Speed of the enter/exit transition
+    transition: true, // Set a transition on enter/exit.
+    axis: 'Y', // What axis should be disabled. Can be X or Y.
+    reset: true, // If the tilt effect has to be reset on exit.
+    easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
+  };
   return (
     <section className="bg-dark-2 mt-[150px] py-6" id="about">
       <h3 className="capitalize font-black text-center text-3xl">about me</h3>
@@ -35,12 +46,17 @@ const About: FC = () => {
         className={`${contentPadding} grid grid-cols-quote-card gap-9 mt-[60px] max-w-[1600px] mx-auto`}
       >
         {quotes.map((quote, index: number) => (
-          <div key={index} className="bg-dark-3 px-[16px] py-[14px] rounded-md duration-300 hover:-translate-y-1">
-            <p>“{quote}”</p>
-            <h5 className="mt-3 flex justify-end italic font-light text-[15px]">
-              - sifedine
-            </h5>
-          </div>
+          <Tilt options={defaultTiltOptions}>
+            <div
+              key={index}
+              className="bg-dark-3 px-[16px] py-[14px] rounded-md duration-300"
+            >
+              <p>“{quote}”</p>
+              <h5 className="mt-3 flex justify-end italic font-light text-[15px]">
+                - sifedine
+              </h5>
+            </div>
+          </Tilt>
         ))}
       </div>
     </section>
