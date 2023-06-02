450e1659f18dbd6fed0c7ec7d5aca2c3940abdd3
dev-pengi <siffffdoul@gmail.com>
2023-06-02 20:45:28 +0100

    Update styles

--- src/styles.ts
+++ src/styles.ts
@@ -4,4 +4,5 @@ const gardient: string = "bg-gradient-to-r from-primary to-primary-2";
 const buttonStyle: string = `${gardient} px-3 py-3 text-[14px] rounded-[5px] cursor-pointer duration-300 hover:scale-[0.95]`;
 const heroIcons: string = `text-primary text-2xl duration-300 hover:translate-y-[-5px]`;
 
+
 export { contentPadding, gardient, buttonStyle, heroIcons };
