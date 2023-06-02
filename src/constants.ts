57ab570cd9f87b9401f248776199a1bb18e387b5
dev-pengi <siffffdoul@gmail.com>
2023-06-02 22:01:40 +0100

    Add skills array

--- src/constants.ts
+++ src/constants.ts
@@ -8,6 +8,10 @@ interface Ihero {
   subContent: string;
   illustration: string;
 }
+interface ISkills {
+  name: string;
+  percentage: number;
+}
 
 const name: string = "Sifedine";
 
@@ -39,6 +43,32 @@ const headerItems: IheaderItem[] = [
     link: "#contact",
   },
 ];
+const skills: ISkills[] = [
+  {
+    name: "Javascript",
+    percentage: 92,
+  },
+  {
+    name: "Typescript",
+    percentage: 62,
+  },
+  {
+    name: "Node js",
+    percentage: 70,
+  },
+  {
+    name: "Express",
+    percentage: 67,
+  },
+  {
+    name: "Mongo DB",
+    percentage: 74,
+  },
+  {
+    name: "React",
+    percentage: 30,
+  },
+];
 
 const aboutDescription: string =
   "i'm a young developer from algeria, i use javascript and typescript i code both client side (react, nextjs) and backend (nodejs express and mongodb) and even server hosting and management. i've shown a good level of dedication and perseverance in pursuing my passion for software development. with a natural ability to solve problems and an eagerness to learn, i'm committed to keeping up to date with the latest technology. my diverse skills in web development, server management, and publishing make me an invaluable asset to any team.";
@@ -49,4 +79,4 @@ const quotes: string[] = [
   "if you're not facing problems at what you're doing, you are not evolving you are repeating",
 ];
 
-export { name, headerItems, aboutDescription, hero, quotes };
+export { name, headerItems, aboutDescription, hero, quotes, skills };
