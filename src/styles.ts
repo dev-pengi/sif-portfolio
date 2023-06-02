5c1849494e44b4222a7cfb2c4cbfb925869bf35c
dev-pengi <siffffdoul@gmail.com>
2023-06-03 00:05:54 +0100

    Fixed a bug

--- src/styles.ts
+++ src/styles.ts
@@ -2,7 +2,7 @@ const contentPadding: string = `px-[15px] lg:px-[90px] md:px-[60px] sm:px-[40px]
 
 const gardient: string = "bg-gradient-to-r from-primary to-primary-2";
 const buttonStyle: string = `${gardient} w-max px-4 py-3 text-[14px] rounded-[5px] cursor-pointer duration-300 hover:scale-[0.95]`;
-const iconButtonStyle: string = `${gardient} w-max w-[45px] h-[45px] flex items-center justify-center text-[24px] rounded-full cursor-pointer duration-300 hover:scale-[0.95]`;
+const iconButtonStyle: string = `${gardient} w-max min-w-[45px] min-h-[45px] flex items-center justify-center text-[24px] rounded-full cursor-pointer duration-300 hover:scale-[0.95]`;
 const heroIcons: string = `text-primary text-2xl duration-300 hover:translate-y-[-5px]`;
 
 export { contentPadding, gardient, buttonStyle, iconButtonStyle, heroIcons };
