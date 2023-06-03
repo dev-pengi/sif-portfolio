9aef2b1e31454d0870c746ac7f5afa70d24e1b76
dev-pengi <siffffdoul@gmail.com>
2023-06-03 22:13:35 +0100

    fixed the id

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -42,7 +42,7 @@ const Contact: FC = () => {
     setLoading(false);
   };
   return (
-    <section className="bg-dark-2 py-12" id="services">
+    <section className="bg-dark-2 py-12" id="contacts">
       <div className={`${contentPadding}`}>
         <div
           className={`max-w-[1400px] mx-auto py-12 px-6 flex items-start lg:flex-row flex-col justify-center gap-9 bg-dark-1 shadow-lg rounded-md`}
