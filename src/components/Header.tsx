af9b1c61d2612ea89036cac142e13d6a24a17674
dev-pengi <siffffdoul@gmail.com>
2023-06-09 20:44:03 +0100

    Add blue effect to the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,46 +26,49 @@ const Header: FC = () => {
   }, []);
 
   return (
-    <header
-      className={`bg-dark-2 top-0 h-14 w-full flex items-center justify-between fixed z-10 ${contentPadding}`}
-    >
-      <a href="#" className="flex items-center justify-center gap-2">
-        <Image src={logo} className="w-[36px]" alt="sifedine | pengi dev" />
-        <h2 className="text-xl">Sifedine</h2>
-      </a>
-      <div ref={wrapperRef}>
-        <ul
-          className={`md:w-max w-full md:bg-transparent bg-dark-3 flex md:flex-row flex-col md:relative md:top-0 md:gap-8 gap-4 fixed duration-200 ${
-            isOpened ? "bottom-[-0.5px]" : "-bottom-64"
-          } left-0 md:rounded-none rounded-tl-lg rounded-tr-lg items-center py-3`}
-        >
-          {headerItems.map((item, index) => (
-            <li key={index} onClick={() => setIsOpened(false)}>
-              <a
-                href={item.link}
-                className="capitalize hover:text-primary duration-300"
-              >
-                {item.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-        <div
-          className="md:hidden block cursor-pointer"
-          id="mobile-burger"
-          onClick={() => setIsOpened(!isOpened)}
-        >
-          {burgers.map((burger, index: number) => (
-            <span
-              key={index}
-              className={`w-[28px] h-[3px] bg-text-color block rounded-lg my-[4px] duration-300 ${
-                isOpened ? "bg-primary" : ""
-              }`}
-            ></span>
-          ))}
+    <>
+      <div className="bg-dark-3 h-14  w-full z-9"></div>
+      <header
+        className={`bg-dark-2-blur top-0 h-14 w-full flex items-center justify-between fixed z-10 backdrop-blur-[25px] ${contentPadding}`}
+      >
+        <a href="#" className="flex items-center justify-center gap-2">
+          <Image src={logo} className="w-[36px]" alt="sifedine | pengi dev" />
+          <h2 className="text-xl">Sifedine</h2>
+        </a>
+        <div ref={wrapperRef}>
+          <ul
+            className={`md:w-max w-full md:bg-transparent bg-dark-3 flex md:flex-row flex-col md:relative md:top-0 md:gap-8 gap-4 fixed duration-200 ${
+              isOpened ? "bottom-[-0.5px]" : "-bottom-64"
+            } left-0 md:rounded-none rounded-tl-lg rounded-tr-lg items-center py-3`}
+          >
+            {headerItems.map((item, index) => (
+              <li key={index} onClick={() => setIsOpened(false)}>
+                <a
+                  href={item.link}
+                  className="capitalize hover:text-primary duration-300"
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div
+            className="md:hidden block cursor-pointer"
+            id="mobile-burger"
+            onClick={() => setIsOpened(!isOpened)}
+          >
+            {burgers.map((burger, index: number) => (
+              <span
+                key={index}
+                className={`w-[28px] h-[3px] bg-text-color block rounded-lg my-[4px] duration-300 ${
+                  isOpened ? "bg-primary" : ""
+                }`}
+              ></span>
+            ))}
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
